c4fbf6c3fc7e5f3e7536abd80682dc25bcdfe11b
kuppharish <kuppamharish28@gmail.com>
2018-09-07 11:55:21 +0800

    test-commit

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
-setup(name='gymfc1',
-      version='0.0.2',
+setup(name='gymfc',
+      version='0.0.1',
       description='Environment for developing intelligent flight control systems',
       author='William Koch',
       author_email='wfkoch [at] bu [dot] edu',
