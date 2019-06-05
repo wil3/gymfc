ff67e8ea4be2fa79cc708a42101fb2605080c07e
Wil <wfkoch@gmail.com>
2019-06-05 09:35:09 -0400

    Fixing constructor

--- tests/test_start_sim.py
+++ tests/test_start_sim.py
@@ -6,8 +6,8 @@ from gymfc.envs.fc_env import FlightControlEnv
 
 
 class Sim(FlightControlEnv):
-    def __init__(self, aircraft_config, config=None):
-        super(self, Sim).__init__(aircraft_config, config_filepath=config)
+    def __init__(self, aircraft_config, config=None, verbose=False):
+        super().__init__(aircraft_config, config_filepath=config, verbose=verbose)
 
     def state(self):
         pass
@@ -25,9 +25,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser("Start the simulator so it can be inspected.")
     parser.add_argument('aircraftconfig', help="File path of the aircraft SDF.")
     parser.add_argument('--gymfc-config', help="Option to override default GymFC configuration location.")
+    parser.add_argument('--verbose', action="store_true")
 
     args = parser.parse_args()
 
-    env = Sim(args.aircraftconfig, args.gymfc_config)
+    env = Sim(args.aircraftconfig, args.gymfc_config, args.verbose)
     env.render()
 
