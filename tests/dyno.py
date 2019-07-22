9294bf5eddfb7e68aaf179c7a826155df1961680
Wil <wfkoch@gmail.com>
2019-07-22 10:13:58 -0400

    Updates to support step and ramp inputs

--- tests/dyno.py
+++ tests/dyno.py
@@ -5,98 +5,63 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 
-def step_input(env, max_command):
+def step_input(env, output_file, max_command, max_sim_time, input_type="step"):
+    """
+        Args:
+            env
+            output_file
+            max_command
+    """
     ob = env.reset()
-    ts = []
-    vels = []
-    commands = []
     command = max_command
+    data = []
+
+    peak_time = max_sim_time/2.0 
+    ramp_steps = peak_time/env.stepsize
+    command_step = 1/ramp_steps
     while True:
         ob = env.step_sim(command)
-        commands.append(command * 100)
-        ts.append(env.sim_time)
-        vels.append(env.esc_motor_angular_velocity)
-        if env.sim_time > 1.0:
-            command = np.zeros(1)
-        if env.sim_time > 2.0:
+        data.append([env.sim_time,command[0], env.esc_motor_angular_velocity, env.esc_force, env.esc_torque ])
+        if input_type == "step":
+            if env.sim_time > peak_time:
+                command = np.zeros(1)
+        else:
+            if env.sim_time < peak_time:
+                command += command_step
+            else:
+                command -= command_step
+
+
+        if env.sim_time > max_sim_time:
             break
+
     env.close()
-
-    #data = np.array(list(zip(ts, vels)))
-    #f = None
-    #np.savetxt(f, data, header="time,velocities", delimiter=",")
-    print ("plotting results")
-    fig, ax = plt.subplots()
-    color = 'r'
-    label = "Motor Signal (%)"
-    ax_command = ax.twinx()
-    ax_command.plot(ts, commands, color=color)
-    ax_command.set_ylabel(label, color=color)
-    ax_command.tick_params(axis='y', labelcolor=color)
-
-    ax.plot(ts, vels)
-    ax.set_xlabel("t")
-    ax.set_ylabel("Motor Velocity")
-    plt.show()
-
-
-def step_sim(env, ac, delay=0):
-    """ Evaluate an environment with the given policy """
-    ob = env.reset()
-    ts = []
-    vels = []
-    while True:
-        print ("Sending ", ac)
-        ob = env.step_sim(ac)
-        ts.append(env.sim_time)
-        vels.append(env.esc_motor_angular_velocity)
-        if env.is_done():
-            break
-    env.close()
-
-    print ("plotting results")
-    plt.plot(ts, vels)
-    plt.xlabel("t")
-    plt.ylabel("Motor Velocity")
-    plt.show()
+    np.savetxt(output_file, data, header="time,command,velocity,force,torque", delimiter=",")
 
 class Sim(FlightControlEnv):
 
-    def __init__(self, aircraft_config, config_filepath=None, max_sim_time=1, verbose=False):
+    def __init__(self, aircraft_config, config_filepath=None, verbose=False):
         super().__init__(aircraft_config, config_filepath=config_filepath, verbose=verbose)
-        self.max_sim_time = max_sim_time
-
-    def state(self):
-        pass
-
-    def desired_state(self):
-        pass
-
-    def is_done(self):
-        return self.sim_time > self.max_sim_time
-
-    def on_observation(self, ob):
-        pass
-
-    def on_reset(self):
-        pass
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser("Step the simulator with the given motor values.")
+    parser = argparse.ArgumentParser("A dyno emulator for validating motor models in simulation.")
+    parser.add_argument('output', help="Output file.")
     parser.add_argument('aircraftconfig', help="File path of the aircraft SDF.")
-    parser.add_argument('value', nargs='+', type=float, help="List of control signals, one for each motor.")
+    parser.add_argument('value', nargs='+', type=float, help="Throttle value for the motor.")
     parser.add_argument('--gymfc-config', default=None, help="Option to override default GymFC configuration location.")
     parser.add_argument('--delay', default=0, type=float, help="Second delay betwee steps for debugging purposes.")
     parser.add_argument('--max-sim-time', default=1, type=float, help="Time in seconds the sim should run for.")
-    parser.add_argument('--verbose', action="store_true")
+    parser.add_argument('--verbose', action="store_true", help="Provide additional logs from Gazebo.")
+    parser.add_argument('--render', action="store_true", help="Display the Gazebo client.")
+    parser.add_argument('--input-type', default="step", help="step or ramp")
 
 
     args = parser.parse_args()
 
-    env = Sim(args.aircraftconfig, config_filepath=args.gymfc_config, max_sim_time=args.max_sim_time, verbose=args.verbose)
-    #env.render()
-    #step_sim(env, np.array(args.value), delay=args.delay)
-    step_input(env, np.array(args.value))
+    env = Sim(args.aircraftconfig, config_filepath=args.gymfc_config, verbose=args.verbose)
+    if args.render:
+        env.render()
+    step_input(env, args.output, np.array(args.value), args.max_sim_time, args.input_type)
 
 
